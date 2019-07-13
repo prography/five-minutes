6773b11ffca4dbeb50b82216de33d8e5dd427e68
Godsenal <tmqps78@gmail.com>
2019-07-13 15:14:59 +0900

    Fix question form to trimming #4

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,21 +4,34 @@ import { IPostQuestion } from '../models/question';
 const MSG = {
   subject: '제목은 두 글자 이상이어야 합니다.',
   content: '질문을 반드시 적어주세요!',
-  code: '코드를 반드시 적어주세요!'
-}
+  code: '코드를 반드시 적어주세요!',
+};
 
-export const subjectSchema = string().required(MSG.subject).min(2);
+export const subjectSchema = string()
+  .trim()
+  .required(MSG.subject)
+  .min(2);
 export const contentSchema = string().required(MSG.content);
 export const codeSchema = string().required(MSG.code);
 
 const questionFormSchema = object({
   subject: subjectSchema,
-  content: string().when('subject', (subject: boolean, schema: Schema<string>) => subject ? contentSchema : schema),
-  code: string().when(['subject', 'content'], (subject: boolean, content: boolean, schema: Schema<string>) => subject && content ? codeSchema : schema),
+  content: string()
+    .trim()
+    .when('subject', (subject: boolean, schema: Schema<string>) =>
+      subject ? contentSchema : schema,
+    ),
+  code: string()
+    .trim()
+    .when(
+      ['subject', 'content'],
+      (subject: boolean, content: boolean, schema: Schema<string>) =>
+        subject && content ? codeSchema : schema,
+    ),
 });
 export const validateQuestionForm = (question: Partial<IPostQuestion>) => {
-  return questionFormSchema.validate(question, { stripUnknown: true, });
-}
+  return questionFormSchema.validate(question, { stripUnknown: true });
+};
 
 export const isEmail = string()
   .email('올바른 Email 형식을 입력해주세요.')
